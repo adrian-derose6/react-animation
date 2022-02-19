a7d2c54a16fc51356b5f00bf9154362ac6bbed5a
Adrian DeRose <adrian.derose6@gmail.com>
2022-02-19 14:08:12 -0600

    Add Transition, TransitionGroup, and CSSTransition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './Modal.css';
 
-const Modal = (props) => {
-	const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
+const animationTiming = {
+	enter: 400,
+	exit: 1000,
+};
 
+const Modal = (props) => {
 	return (
-		<div className={cssClasses.join(' ')}>
-			<h1>A Modal</h1>
-			<button className='Button' onClick={props.closed}>
-				Dismiss
-			</button>
-		</div>
+		<CSSTransition
+			in={props.show}
+			timeout={animationTiming}
+			mountOnEnter
+			unmountOnExit
+			classNames={{
+				enter: '',
+				enterActive: 'ModalOpen',
+				exit: '',
+				exitActive: 'ModalClosed',
+			}}
+		>
+			<div className='Modal'>
+				<h1>A Modal</h1>
+				<button className='Button' onClick={props.closed}>
+					Dismiss
+				</button>
+			</div>
+		</CSSTransition>
 	);
 };
 
